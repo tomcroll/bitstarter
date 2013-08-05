e3bdc19e21076528e0de1c1061b5f9d36a7ed337
Tom Croll <tomcroll@gmail.com>
2013-08-05 15:50:30 +0000

    Added buffer and file read variables

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var app = express();
+
 var fs = require('fs');
-var text = fs.readFileSync('index.html','utf8');
+
+var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(text);
+  var buf = fs.readFileSync('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
