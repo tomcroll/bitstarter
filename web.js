3a63637f57b3cce3f9c18cc77a3c069428fcb20b
Tom Croll <tomcroll@gmail.com>
2013-08-05 15:12:47 +0000

    Streaming from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var text = fs.readFileSync('index.html','utf8');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
